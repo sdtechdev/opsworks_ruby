60eb90b76b65b57c5d716d1a56a8f40d3c558e96
Shkrt <zxcgpppmnn@gmail.com>
2022-07-20 17:40:27 +0700

    Add cron jobs to handle periodic updates

--- apt_periodic_updates/recipes/use_cron.rb
+++ apt_periodic_updates/recipes/use_cron.rb
@@ -16,4 +16,17 @@ node[:deploy].each do |app_name, deploy|
   systemd_unit 'apt-daily-upgrade.timer' do
     action :mask
   end
+
+  cron 'apt-daily' do
+    hour '6'
+    user 'root'
+    command '/usr/lib/apt/apt-helper wait-online; /usr/lib/apt/apt.systemd.daily update'
+  end
+
+  cron 'apt-daily-upgrade' do
+    hour '6,18'
+    minute '10'
+    user 'root'
+    command '/usr/lib/apt/apt.systemd.daily install'
+  end
 end
