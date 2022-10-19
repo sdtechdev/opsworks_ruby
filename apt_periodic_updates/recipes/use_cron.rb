cfa5763d71d9d7b5f7023bbda0fe9a9e2fb9d7f2
Shkrt <zxcgpppmnn@gmail.com>
2022-10-19 21:05:57 +0700

    Specify path for apt-daily crontab tasks

--- apt_periodic_updates/recipes/use_cron.rb
+++ apt_periodic_updates/recipes/use_cron.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+default_path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+
 node[:deploy].each do |app_name, deploy|
   systemd_unit 'apt-daily' do
     action :mask
@@ -18,14 +20,16 @@ node[:deploy].each do |app_name, deploy|
   end
 
   cron 'apt-daily' do
-    hour '6'
+    path default_path
+    hour '*/2'
     user 'root'
     command '/usr/lib/apt/apt-helper wait-online; /usr/lib/apt/apt.systemd.daily update'
   end
 
   cron 'apt-daily-upgrade' do
-    hour '6,18'
-    minute '10'
+    path default_path
+    hour '*/2'
+    minute '12'
     user 'root'
     command '/usr/lib/apt/apt.systemd.daily install'
   end
