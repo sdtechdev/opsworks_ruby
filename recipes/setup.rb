38dfa7921312efdb620f2f1d4318486f8c67f4f0
Rifqi Fauzi <7533952+ikirifqi@users.noreply.github.com>
2023-11-08 08:22:02 +0000

    Install JPEG ImageMagick decode delegate

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -388,7 +388,7 @@ if node['platform_family'] == 'debian'
 end
 
 # Imagemagick Installation
-apt_package 'libpng-dev'
+apt_package %w[libpng-dev libjpeg-dev]
 imagemagick_archive_path = ::File.join(Chef::Config[:file_cache_path], 'imagemagick-6.9.10-97.tar.xz')
 
 
